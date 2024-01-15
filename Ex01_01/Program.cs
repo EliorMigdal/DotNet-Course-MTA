5d2b5ce2b1f6326dfdecae786cebe55d2d862970
Elior Migdal <eliormi2@mta.ac.il>
2024-01-15 11:35:10 +0200

    Ex01_01 fully implemented.

--- Ex01_01/Program.cs
+++ Ex01_01/Program.cs
@@ -1,5 +1,4 @@
 ﻿using static System.Console;
-//Check
 
 namespace Ex01_01
 {
@@ -7,38 +6,46 @@ namespace Ex01_01
     {
         public static void Main(string[] args)
         {
-            runProgram();
+            RunProgram();
         }
 
-        public static void runProgram()
+        public static void RunProgram()
         {
             WriteLine($"Welcome to our program!\nPlease enter 3 9-digit positive binary numbers.");
             WriteLine("First number: ");
-            int firstDecimal = readInputAndConvertToDecimal();
+            int firstDecimal = ReadBinaryInputAndConvertToDecimal(out string firstInput);
 
             WriteLine("Second number: ");
-            int secondDecimal = readInputAndConvertToDecimal();
+            int secondDecimal = ReadBinaryInputAndConvertToDecimal(out string secondInput);
 
             WriteLine("Third number: ");
-            int thirdDecimal = readInputAndConvertToDecimal();
+            int thirdDecimal = ReadBinaryInputAndConvertToDecimal(out string thirdInput);
+
+            WriteLine("Some statistics about your input:");
+            WriteLine($"The average number of zeroes in your input is {(float)((float)(NumOfZeroes(firstInput) + NumOfZeroes(secondInput) + NumOfZeroes(thirdInput)) / 3)}");
+            WriteLine($"The average number of ones in your input is {(float) ((float) (NumOfOnes(firstInput) + NumOfOnes(secondInput) + NumOfOnes(thirdInput)) / 3)}");
+            WriteLine($"You have entered {NumOfPowerOfTwo(firstDecimal, secondDecimal, thirdDecimal)} numbers that are power of 2.");
+            WriteLine($"You have entered {NumOfAscendingSeries(firstDecimal, secondDecimal, thirdDecimal)} numbers that their digits are an ascending seris.");
+            WriteLine($"The maximum of your inputs is: {Math.Max(Math.Max(firstDecimal, secondDecimal), thirdDecimal)}");
+            WriteLine($"The minimum of your inputs is: {Math.Min(Math.Min(firstDecimal, secondDecimal), thirdDecimal)}");
         }
 
-        public static int readInputAndConvertToDecimal()
+        public static int ReadBinaryInputAndConvertToDecimal(out string o_Input)
         {
-            string input = ReadLine();
+            o_Input = ReadLine();
 
-            while (isInputValid(input) == false)
+            while (isInputValid(o_Input) == false)
             {
-                WriteLine($"{input} is an invalid input! Please enter a 9-digit positive binary number.");
-                input = ReadLine();
+                WriteLine($"{o_Input} is an invalid input! Please enter a 9-digit positive binary number.");
+                o_Input = ReadLine();
             }
 
-            return convertBinaryToDecimal(input);
+            return ConvertBinaryToDecimal(o_Input);
         }
 
-        public static bool isInputValid(string i_Input)
+        private static bool isInputValid(string i_Input)
         {
-            bool isInputValid = true;
+            bool isInputValid = i_Input.Length == 9;
 
             for (int i = 0; i < i_Input.Length && isInputValid == true; i++)
             {
@@ -51,7 +58,7 @@ namespace Ex01_01
             return isInputValid;
         }
 
-        public static int convertBinaryToDecimal(string i_Binary)
+        public static int ConvertBinaryToDecimal(string i_Binary)
         {
             int decimalNumber = 0;
 
@@ -59,11 +66,103 @@ namespace Ex01_01
             {
                 if (i_Binary[i] == '1')
                 {
-                    decimalNumber += (int) Math.Pow(2, i_Binary.Length - i);
+                    decimalNumber += (int) Math.Pow(2, i_Binary.Length - (i + 1));
                 }
             }
 
             return decimalNumber;
+        }
+
+        public static int NumOfZeroes(string i_Binary)
+        {
+            int numOfZeroes = 0;
+
+            for (int i = 0; i < i_Binary.Length; i++)
+            {
+                if (i_Binary[i] == '0')
+                {
+                    numOfZeroes++;
+                }
+            }
+
+            return numOfZeroes;
+        }
+
+        public static int NumOfOnes(string i_Binary)
+        {
+            return 9 - NumOfZeroes(i_Binary);
+        }
+
+        public static short NumOfPowerOfTwo(int i_FirstInput, int i_SecondInput, int i_ThirdInput)
+        {
+            short output = 0;
+            
+            if (IsPowerOfTwo (i_FirstInput))
+            {
+                output++;
+            }
+
+            if (IsPowerOfTwo(i_SecondInput))
+            {
+                output++;
+            }
+
+            if (IsPowerOfTwo(i_ThirdInput))
+            {
+                output++;
+            }
+
+            return output;
+        }
+
+        public static bool IsPowerOfTwo(int i_Number)
+        {
+            return Math.Log2(i_Number) - (int) Math.Log2(i_Number) == 0;
+        }
+
+        public static short NumOfAscendingSeries(int i_FirstInput, int i_SecondInput, int i_ThirdInput)
+        {
+            short output = 0;
+
+            if (IsAnAscendingSeries (i_FirstInput))
+            {
+                output++;
+            }
+
+            if (IsAnAscendingSeries(i_SecondInput))
+            {
+                output++;
+            }
+
+            if (IsAnAscendingSeries(i_ThirdInput))
+            {
+                output++;
+            }
+
+            return output;
+
+        }
+
+        public static bool IsAnAscendingSeries(int i_Number)
+        {
+            bool isAscending = true;
+            int currentDigit = i_Number % 10, previousDigit;
+            i_Number /= 10;
+
+            while (i_Number > 0 && isAscending == true)
+            {
+                previousDigit = currentDigit;
+                currentDigit = i_Number % 10;
+
+                if (previousDigit <= currentDigit)
+                {
+                    isAscending = false;
+                }
+
+                i_Number /= 10;
+            }
+
+            return isAscending;
         }
     }
 }
