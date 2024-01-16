f81c55f4af0dde942324182b402bcbb7728d7920
EliorMigdal <104457430+EliorMigdal@users.noreply.github.com>
2024-01-16 07:46:52 +0200

    Update Program.cs on Ex01_04

--- Ex01_04/Program.cs
+++ Ex01_04/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+using System.Text;
+using static System.Console;
 
 namespace Ex01_04
 {
@@ -11,17 +12,22 @@ namespace Ex01_04
 
         public static void RunProgram(string i_String)
         {
-            WriteLine($"Is {i_String} a palindrome? {(IsPalindrome(i_String) ? "Yes!" : "No!")}");
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(string.Format("Some data about {0}:\n", i_String));
+
+            stringBuilder.Append(string.Format("{0} is{1} a palindrome.\n", i_String, IsPalindrome(i_String) ? "" : " not"));
 
             if (IsANumber(i_String, out int parsedNumber) == true)
             {
-                WriteLine($"Does {i_String} divide by 5? {(parsedNumber % 5 == 0 ? "Yes!" : "No!")}");
+                stringBuilder.Append(string.Format("{0} does{1} divide by 5.\n", i_String, parsedNumber % 5 == 0 ? "" : " not"));
             }
 
             else
             {
-                WriteLine($"Number of lowercase letters in {i_String}: {CountLowerCase(i_String)}");
+                stringBuilder.Append(string.Format("{0} has {1} lowercase letters.\n", i_String, CountLowerCase(i_String)));
             }
+
+            Write(stringBuilder.ToString());
         }
 
         public static bool IsPalindrome(string i_String)
@@ -49,7 +55,7 @@ namespace Ex01_04
 
             for (int i = 0; i < i_String.Length; i++)
             {
-                if (i_String[i] >= 'a' && i_String[i] <= 'z')
+                if (char.IsLower(i_String[i]) == true)
                 {
                     output++;
                 }
