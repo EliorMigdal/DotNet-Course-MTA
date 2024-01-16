1c4eee0b1149bdcc3510714859be36062db7e9e2
EliorMigdal <104457430+EliorMigdal@users.noreply.github.com>
2024-01-16 08:01:57 +0200

    Update Program.cs on Ex01_05

--- Ex01_05/Program.cs
+++ Ex01_05/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System.Text;
 using static System.Console;
 using static System.Math;
 
@@ -13,22 +13,51 @@ namespace Ex01_05
 
         public static void RunProgram()
         {
-            int input = ReadInputAndParse();
-
-            Console.WriteLine($"Some statistics regarding the number {input}:");
-            Console.WriteLine($"1. The largest digit in {input} is {ExtractMaxDigit(input)}");
-            Console.WriteLine($"2. The smallest digit in {input} is {ExtractMinDigit(input)}");
-            Console.WriteLine($"3. The number of digits in {input} that divide by 4 is {CountDividingByFour(input)}");
-            Console.WriteLine($"4. The number of digits in {input} that are bigger than its unity digit, {input % 10}, is {CountNumOfGreaterThanUnityDigit(input)}");
-            Console.WriteLine($"5. The average of {input}'s digits is {CalculateDigitsAverage(input)}");
-        }
-
-        public static int ReadInputAndParse()
-        {
-            WriteLine("Hi there! Please enter a 9-digit number:");
+            Write("Hi there! Please enter a 9-digit number: ");
             string input = ReadLine();
 
-            return int.Parse(input);
+            while (IsInputValid(input) == false)
+            {
+                HandleInvalidInput(ref input);
+            }
+
+            int parsedInput = int.Parse(input);
+            DisplayStatistics(parsedInput);
+        }
+
+        public static bool IsInputValid(string i_Input)
+        {
+            bool isInputValid = i_Input.Length == 9;
+
+            for (int i = 0; i < i_Input.Length && isInputValid == true; i++)
+            {
+                if (char.IsDigit(i_Input[i]) == false)
+                {
+                    isInputValid = false;
+                }
+            }
+
+            return isInputValid;
+        }
+
+        public static void HandleInvalidInput(ref string io_Input)
+        {
+            Write($"{io_Input} is an invalid input! Please enter a 9-digit positive number: ");
+            io_Input = ReadLine();
+        }
+
+        public static void DisplayStatistics(int i_Input)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine(string.Format("\nSome statistics regarding the number {0}:", i_Input));
+            stringBuilder.AppendLine(string.Format("1. The largest digit in {0} is {1}", i_Input, ExtractMaxDigit(i_Input)));
+            stringBuilder.AppendLine(string.Format("2. The smallest digit in {0} is {1}", i_Input, ExtractMinDigit(i_Input)));
+            stringBuilder.AppendLine(string.Format("3. The number of digits in {0} that divide by 4 is {1}", i_Input, CountDividingByFour(i_Input)));
+            stringBuilder.AppendLine(string.Format("4. The number of digits in {0} that are bigger than its unity digit, {1}, is {2}", i_Input, i_Input % 10, CountNumOfGreaterThanUnityDigit(i_Input)));
+            stringBuilder.AppendLine(string.Format("5. The average of {0}'s digits is {1}", i_Input, CalculateDigitsAverage(i_Input)));
+
+            Write(stringBuilder.ToString());
         }
 
         public static int ExtractMaxDigit(int i_Number)
